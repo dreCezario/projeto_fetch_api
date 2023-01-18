42cf982ae409ee8143d293797ad7a77a255f4e3b
André <developer.andrecezario@gmail.com>
2023-01-18 11:16:51 -0300

    adicionando a função para pegar o post individual

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ const url = "https://jsonplaceholder.typicode.com/posts"
 const loadingElement = document.querySelector('#loading')
 const postsContainer = document.querySelector('#posts-container')
 
+const postPage = document.querySelector('#post')
+const postContainer = document.querySelector('#post-container')
+const commentsContainer = document.querySelector('#comments-container')
+
+// Get id from URL
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get("id")
+
 // Get all posts
 
 async function getAllPosts() {
     const response = await fetch(url)
+
+    console.log(response)
 
     const data = await response.json()
     
@@ -29,9 +39,27 @@ async function getAllPosts() {
         div.appendChild(title)
         div.appendChild(body)
         div.appendChild(link)
-
         postsContainer.appendChild(div)
     })
 }
 
-getAllPosts()
+// Get individual post
+
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all([
+        fetch(`${url}/${id}`),
+        fetch(`${url}/${id}/comments`),
+    ])
+
+    const dataPost = await responsePost.json()
+    const dataComments = await responseComments.json()
+
+    loadingElement.classList.add('hide')
+    postPage.classList.remove('hide')
+}
+
+if(!postId){
+    getAllPosts()   
+} else{
+    getPost(postId);
+}
